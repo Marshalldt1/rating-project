3c096f6f04fc2560712c37f9b50832699d556e07
Marshalldt1 <leonardotera92@gmail.com>
2022-08-04 12:32:49 -0300

    finish project

---
+++ README.md
@@ -0,0 +1,65 @@
+# Frontend Mentor - Interactive rating component solution
+
+This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+- If no rating is selected the submit button would be disabled
+
+### Screenshot
+
+![](./screenshot-desktop.png)
+![](./screenshot-mobile.png)
+![](./screenshot-mobile1.png)
+
+
+
+
+### Links
+
+- Solution URL: [Github solution URL here](https://github.com/Marshalldt1/rating-project)
+- Live Site URL: [ live site URL here](marshalldt1.github.io/rating-project/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- DOM javascript
+
+### What I learned
+
+Javascript skills
+
+## Author
+
+- Frontend Mentor - [@tchydy](https://www.frontendmentor.io/profile/Marshalldt1)
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,27 +1,41 @@
+// variables and selectores
 const button = document.querySelectorAll(".buttons button");
-const buttonArray = Array.from(button);
+const sections = document.querySelectorAll("section");
+const submit = document.querySelector(".submit-btn");
+const divNota = document.querySelector(".selected-rating");
+let score;
 
+// function for add classe for button
 function ativarBtn(index) {
   button.forEach((item) => {
     item.classList.remove("ative-btn");
   });
   button[index].classList.add("ative-btn");
 }
+// add classe to button
 button.forEach((item, index) => {
   item.addEventListener("click", () => {
     ativarBtn(index);
+    score = index + 1;
   });
-});
 
-const sections = document.querySelectorAll("section");
-const submit = document.querySelector(".submit-btn");
-const divNota = document.querySelector(".selected-rating");
-
-sections[0].classList.add("ative");
-
-submit.addEventListener("click", (item) => {
-  sections.forEach((item) => {
-    item.classList.toggle("ative");
+  // add class "ative for changer"
+  sections[0].classList.add("ative");
+  // event for submit click
+  submit.addEventListener("click", () => {
+    //check if button 1,2,3,4 or 5 is clicked
+    // if not make button unclickable
+    for (let i = 0; i < button.length; i++) {
+      if (button[i].classList.contains("ative-btn")) {
+        score = i + 1;
+        // hide rating and show thank you container
+        sections.forEach((item) => {
+          item.classList.toggle("ative");
+        });
+        console.log(score);
+      }
+    }
+    // score value
+    divNota.innerHTML = "You selected " + score + " out of 5";
   });
-  divNota.innerHTML = "You selected " + score + " out of 5";
 });
